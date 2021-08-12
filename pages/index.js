938f6a37be12f5dbaff3535bca6cd7d1a5ccb9a9
Arecio Canton <cuba289@gmail.com>
2021-08-12 00:40:55 -0400

    dummy contact section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import Login from '../components/Login';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
+import Widget from '../components/Widget';
 
 export default function Home({ session }) {
   if (!session) return <Login />;
@@ -18,6 +19,7 @@ export default function Home({ session }) {
       <main className="flex">
         <Sidebar />
         <Feed />
+        <Widget />
       </main>
 
     </div>
